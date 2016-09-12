fd2528898f251809051b9491dae8d5bc72737bdd
drafteee <sergej_-96@mail.ru>
2016-09-12 23:02:13 +0300

    QuickSort

--- QuickSortNS/ConsoleQuickSort/Program.cs
+++ QuickSortNS/ConsoleQuickSort/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuickSortNS;
 
 namespace ConsoleQuickSort
 {
@@ -10,6 +11,21 @@ namespace ConsoleQuickSort
     {
         static void Main(string[] args)
         {
+            Console.Write("Input amount of numbers:");
+
+            int amountOfNumbers = Convert.ToInt32(Console.ReadLine());
+            QuickSort sort = new QuickSort(amountOfNumbers);
+
+            sort.QSort(0, amountOfNumbers - 1);
+
+            int[] array = sort.GetStringValues();
+
+            Console.Write("Sorted array:");
+            for(int i = 0; i < amountOfNumbers; i++)
+            {
+                Console.Write(" {0}",Convert.ToString(array[i]));
+            }
+
             Console.Read();
         }
     }
--- QuickSortNS/QuickSortNS/QuickSort.cs
+++ QuickSortNS/QuickSortNS/QuickSort.cs
@@ -8,9 +8,57 @@ namespace QuickSortNS
 {
     public class QuickSort
     {
-        public QuickSort()
+        private int amountGenericValues;
+        private int[] arrayRandomValues;
+        public QuickSort(int amountOfNumbers)
         {
-            //int a = Console.Read();
+            amountGenericValues = amountOfNumbers;
+            arrayRandomValues = new int[amountGenericValues];
+
+            GenericArray();
+        }
+
+        public void GenericArray()
+        {
+            Random random = new Random();
+
+            for(int i = 0; i < amountGenericValues; i++)
+            {
+                arrayRandomValues[i] = random.Next(amountGenericValues);
+            }
+        }
+
+        public int[] GetStringValues()
+        {
+            return arrayRandomValues;
+        }
+        public void QSort(int l,int r)
+        {
+            int temp;
+            int x = arrayRandomValues[l + (r - l) / 2];
+            //запись эквивалентна (l+r)/2, 
+            //но не вызввает переполнения на больших данных
+            int i = l;
+            int j = r;
+            //код в while обычно выносят в процедуру particle
+            while (i <= j)
+            {
+                while (arrayRandomValues[i] < x) i++;
+                while (arrayRandomValues[j] > x) j--;
+                if (i <= j)
+                {
+                    temp = arrayRandomValues[i];
+                    arrayRandomValues[i] = arrayRandomValues[j];
+                    arrayRandomValues[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < r)
+                QSort(i, r);
+
+            if (l < j)
+                QSort(l, j);
         }
     }
 }
