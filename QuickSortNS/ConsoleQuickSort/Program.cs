ec428f51953d5cb1bcdd0b7625b43257df67a807
drafteee <sergej_-96@mail.ru>
2016-09-12 20:56:18 +0300

    QuickSort

--- QuickSortNS/ConsoleQuickSort/Program.cs
+++ QuickSortNS/ConsoleQuickSort/Program.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuickSortNS
+namespace ConsoleQuickSort
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
     }
 }
---
+++ QuickSortNS/QuickSortNS/QuickSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortNS
+{
+    public class QuickSort
+    {
+        public QuickSort()
+        {
+            //int a = Console.Read();
+        }
+    }
+}
